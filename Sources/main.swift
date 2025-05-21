c7d10fe63bf1b1acbebec02fd09b43af336e9da0
carlaCP <64612629+carlaCP@users.noreply.github.com>
2025-05-21 18:43:08 +0000

    exercicios _ 21/05

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-var nome = "Seu Nome"
+/*var nome = "Seu Nome"
 let sobrenome = "Seu Sobrenome"
 
-print("Bem-vindo(a), \(nome) \(sobrenome)!")
+print("Bem-vindo(a), \(nome) \(sobrenome)!")*/
+
+import Foundation
+
+/*var a = 64.0
+
+var raizCubo = cbrt(a)
+print("raiz cubica: \(a) = \(raizCubo)")
+
+raizCubo = pow(a, 1/3)
+print(raizCubo)*/
+
+//exercicio 1
+
+print("Entre com a temperatura em Celsius")
+var celsius = (Double(readLine()!)!)
+
+var fahrenheit = pow(celsius,9/5)
+
